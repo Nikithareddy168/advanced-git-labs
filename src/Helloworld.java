bd2977be308ea8bf1e8483176fe70e880a784fda
Nikithareddy168 <nikitha.reddy168@gmail.com>
2018-05-02 09:41:02 -0400

    first commit

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,7 +3,8 @@ public class Helloworld {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello conflicts from sandbox !!");
+       // System.out.println("Hello conflicts from sandbox !!");
+        System.out.println("My favourite band is One direction");
     }
 
 }
